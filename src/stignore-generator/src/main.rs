e749803a20c47ffd324504d9dfa6ed95ccc9bf2d
117503445 <t117503445@gmail.com>
2024-01-02 02:43:24 +0800

    feat: use notify
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/stignore-generator/src/main.rs
+++ src/stignore-generator/src/main.rs
@@ -1,3 +1,5 @@
+use notify::Config;
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use std::env;
 use std::path::Path;
 
@@ -5,9 +7,6 @@ use env_logger::{Builder, Env};
 use log::info;
 use stignore_generator::write_stgitignore;
 use stignore_generator::write_stignore;
-
-extern crate inotify;
-use inotify::{Inotify, WatchMask};
 
 fn write() {
     let workspace_dir: &Path = Path::new(".");
@@ -26,30 +25,37 @@ fn main() {
     let workspace_path = Path::new("/workspace");
 
     env::set_current_dir(workspace_path).unwrap();
-    let mut inotify = Inotify::init().expect("Failed to initialize inotify");
 
-    inotify
-        .watches()
-        .add(
-            workspace_path,
-            WatchMask::MODIFY | WatchMask::CREATE | WatchMask::DELETE,
-        )
-        .expect("Failed to add inotify watch");
+    // write();
 
-    write();
-        
-    let mut buffer = [0u8; 4096];
-    loop {
-        let events = inotify
-            .read_events_blocking(&mut buffer)
-            .expect("Failed to read inotify events");
+    if let Err(error) = watch(workspace_path) {
+        log::error!("Error: {error:?}");
+    }
+}
 
-        for event in events {
-            if event.name.is_some() {
-                if event.name.unwrap() == ".gitignore" {
-                    write();
+fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => {
+                for path in event.paths {
+                    if path.display().to_string().ends_with(".gitignore") {
+                        write();
+                    }
                 }
             }
+            Err(error) => log::error!("Error: {error:?}"),
         }
     }
+
+    Ok(())
 }
